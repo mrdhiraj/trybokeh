8d31bae417d36d6208aaf0df2b42c0fc898a718b
dj <dj@dj.com>
2020-05-18 20:41:49 +0545

    button

--- BokehServer/fullyworks try1.py
+++ BokehServer/fullyworks try1.py
@@ -3,10 +3,6 @@ from bokeh.io import curdoc
 from bokeh.plotting import figure
 
 # Create a new plot: plot
-plot = figure()
-
-# Add a line to the plot
-plot.line([1,2,3,4,5],[2,5,4,6,7])
-
+xzc
 # Add the plot to the current document
 curdoc().add_root(plot)
