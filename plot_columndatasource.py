8088fb5f6b996d696fc9445a640832e2aaf25a99
dj <dj@dj.com>
2020-05-18 19:08:54 +0545

    somethings interacitve

---
+++ plot_columndatasource.py
@@ -0,0 +1,12 @@
+# Import the ColumnDataSource class from bokeh.plotting
+from bokeh.plotting import ColumnDataSource
+
+# Create a ColumnDataSource: source
+source = ColumnDataSource(df)
+
+# Add circle glyphs to the figure p
+p.circle(x='Year', y='Time', color='color', size=8, source=source)
+
+# Specify the name of the output file and show the result
+output_file('sprint.html')
+show(p)
