ad6d9487bc2926484bfa82edababe5590be10eca
dj <dj@dj.com>
2020-05-18 18:46:57 +0545

    clean

---
+++ 3plot_numpy.py
@@ -0,0 +1,16 @@
+ import numpy as np
+
+
+# Create array using np.linspace: x
+x = np.linspace(0,5,100)
+
+# Create array using np.cos: y
+y = np.cos(x)
+
+# Add circles at x and y
+
+p.circle(x,y)
+
+# Specify the name of the output file and show the result
+output_file('numpy.html')
+show(p)
