c4ae5ad9a828164e87827a71aa7a904fa95b3231
dj <dj@dj.com>
2020-05-18 20:08:34 +0545

    bokeh server

---
+++ BokehServer/multiplewidget.py
@@ -0,0 +1,17 @@
+# Perform the necessary imports
+from bokeh.io import curdoc
+from bokeh.plotting import figure
+from bokeh.layouts import widgetbox
+from bokeh.models import Slider
+
+# Create first slider: slider1
+slider1 = Slider(title='my slider', start=0, end=10, step=0.1, value=2)
+
+# Create second slider: slider2
+slider2 = Slider(title='my slider', start=10, end=100, step=1, value=20)
+
+# Add slider1 and slider2 to a widgetbox
+layout = widgetbox(slider1,slider2)
+
+# Add the layout to the current document
+curdoc().add_root(layout)
